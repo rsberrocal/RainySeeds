9eb8cbb8cf9e7a6e701485c6c0038ad90af2df70
Katia <katia.gaag@gmail.com>
2020-03-15 16:47:38 +0100

    login background and new fragment menu

---
+++ app/src/main/java/com/rainyteam/rainyseeds/SelectionViewMenu.kt
@@ -0,0 +1,18 @@
+package com.rainyteam.rainyseeds
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SelectionViewMenu : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_viewmenu, container, false)
+    }
+}
