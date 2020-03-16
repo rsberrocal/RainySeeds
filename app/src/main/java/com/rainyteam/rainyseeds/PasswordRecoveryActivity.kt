d7ac42d04c091afe4a60e6f7d62b93a91dd5ea03
Richard <rsberrocal@gmail.com>
2020-03-16 20:27:05 +0100

    Navigation on pass recovery

--- app/src/main/java/com/rainyteam/rainyseeds/PasswordRecoveryActivity.kt
+++ app/src/main/java/com/rainyteam/rainyseeds/PasswordRecoveryActivity.kt
@@ -8,16 +8,18 @@ import android.widget.Button
 
 class PasswordRecoveryActivity : AppCompatActivity() {
 
-    val btnReturnLogin = findViewById<View>(R.id.btnReturnLogin) as Button
-    val btnSendPass = findViewById<View>(R.id.btnSendPassword) as Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.password_recovery_layout)
 
+        val btnReturnLogin = findViewById<View>(R.id.btnReturnLogin) as Button
+        val btnSendPass = findViewById<View>(R.id.btnSendPassword) as Button
+
         btnReturnLogin.setOnClickListener(View.OnClickListener {
-            val returnLogin = Intent(this, LoginActivity::class.java)
-            startActivity(returnLogin)
+            /*val returnLogin = Intent(this, LoginActivity::class.java)
+            startActivity(returnLogin)*/
+            /** En vez de crear un nuevo actvity, este se cierra **/
+            finish();
         })
 
         btnSendPass.setOnClickListener(View.OnClickListener { view ->
