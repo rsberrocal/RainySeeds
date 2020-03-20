6fbda512cf1283f7970f32567efa91e428779a3f
SergiFernandez99 <sfernaga79@ub.alumnes.edu>
2020-03-20 21:59:10 +0100

    Added main_water background, created activity and layout, and setted button position.

---
+++ app/src/main/java/com/rainyteam/rainyseeds/MainWaterActivity.kt
@@ -0,0 +1,12 @@
+package com.rainyteam.rainyseeds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainWaterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout._main_water_layout)
+    }
+}
