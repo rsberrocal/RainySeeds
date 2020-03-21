a692fa9af80c79682e0faa55f0d7eac066e42e11
Richard <rsberrocal@gmail.com>
2020-03-21 21:43:56 +0100

    Created Plant detail

---
+++ app/src/main/java/com/rainyteam/controller/EncyclopediaDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.rainyteam.controller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EncyclopediaDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.encyclopedia_detail_layout)
+
+    }
+}
