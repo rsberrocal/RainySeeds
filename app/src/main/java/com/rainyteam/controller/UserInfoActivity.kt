7dfe81f12888c20b752d5f3a4b1905fc0270313f
SergiFernandez99 <sfernaga79@ub.alumnes.edu>
2020-03-22 01:09:25 +0100

    Created and setted UserInfoActivity/Layout.

---
+++ app/src/main/java/com/rainyteam/controller/UserInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.rainyteam.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_info_layout)
+    }
+}
