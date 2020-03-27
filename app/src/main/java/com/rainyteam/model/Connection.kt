2e05eae389695a36ea16738aa771e4b9a70f3efd
Ruben <rubenpa97@gmail.com>
2020-03-27 19:04:18 +0100

    Connection - Start Singleton

--- app/src/main/java/com/rainyteam/model/Connection.kt
+++ app/src/main/java/com/rainyteam/model/Connection.kt
@@ -1,4 +1,27 @@
 package com.rainyteam.model
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 class Connection {
+    private var database: DatabaseReference
+
+    //Singleton, creem una nova instància si no hi ha cap creada anteriorment.
+    init {
+        database = FirebaseDatabase.getInstance().reference
+    }
+
+    fun main(args: Array<String>) {
+
+    }
+
+    fun mAuth(): FirebaseAuth {    //Para la autentificación
+        return FirebaseAuth.getInstance()
+    }
+
+    fun mDatabase(): DatabaseReference {    //Para leer o escribir en la base de datos
+        return database
+    }
+
 }
