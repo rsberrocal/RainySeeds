adc991b3cdfaa46c4a5149dc6272270b94a31fdc
SergiFernandez99 <sfernaga79@ub.alumnes.edu>
2020-03-28 02:06:03 +0100

    Created User Class

--- app/src/main/java/com/rainyteam/model/User.kt
+++ app/src/main/java/com/rainyteam/model/User.kt
@@ -1,4 +1,51 @@
 package com.rainyteam.model
 
-class User {
+import com.rainyteam.views.Plant
+import kotlin.String as String
+import kotlin.collections.ArrayList as ArrayList
+
+//Default constructor
+class User(
+    private var nombre: String,
+    private var email: String,
+    private var password: String,
+    private var age: Int,
+    private var weight: Int,
+    private var height: Float,
+    private var sex: String,
+    private var exercise: Int
+) {
+    var cartera: Int
+    var plantList: ArrayList<String>
+
+    init {
+        this.cartera = 0
+        this.plantList = ArrayList<String>()
+    }
+    //Getters
+    fun getEmail() :String {
+        return this.email
+    }
+    fun getName() :String {
+        return this.nombre
+    }
+    fun getPassword() :String {
+        return this.password
+    }
+    fun getWeight() :Int {
+        return this.weight
+    }
+    fun getAge() :Int {
+        return this.age
+    }
+    fun getHeight() :Float {
+        return this.height
+    }
+    fun getSex() :String {
+        return this.sex
+    }
+    fun getExercise() :Int {
+        return this.exercise
+    }
 }
+
--- app/src/main/java/com/rainyteam/views/Plant.kt
+++ app/src/main/java/com/rainyteam/views/Plant.kt
@@ -1,4 +1,4 @@
-package com.rainyteam.model
+package com.rainyteam.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
