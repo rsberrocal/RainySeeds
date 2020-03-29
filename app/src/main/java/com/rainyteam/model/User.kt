5480e6c73d1fb9eded44e608b1afbb5f2caecfb8
Katia <katia.gaag@gmail.com>
2020-03-29 14:06:53 +0200

    variables en ingles

--- app/src/main/java/com/rainyteam/model/User.kt
+++ app/src/main/java/com/rainyteam/model/User.kt
@@ -1,12 +1,11 @@
 package com.rainyteam.model
 
-import com.rainyteam.views.Plant
 import kotlin.String as String
 import kotlin.collections.ArrayList as ArrayList
 
 //Default constructor
 class User(
-    private var nombre: String,
+    private var username: String,
     private var email: String,
     private var password: String,
     private var age: Int,
@@ -15,19 +14,19 @@ class User(
     private var sex: String,
     private var exercise: Int
 ) {
-    var cartera: Int
+    var wallet: Int
     var plantList: ArrayList<String>
 
     init {
-        this.cartera = 0
+        this.wallet = 0
         this.plantList = ArrayList<String>()
     }
     //Getters
     fun getEmail() :String {
         return this.email
     }
-    fun getName() :String {
-        return this.nombre
+    fun getUsername() :String {
+        return this.username
     }
     fun getPassword() :String {
         return this.password
