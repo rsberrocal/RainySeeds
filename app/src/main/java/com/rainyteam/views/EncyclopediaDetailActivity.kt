2b334edf7305384b9c6e6ee9772a08d656374b75
Katia <katia.gaag@gmail.com>
2020-04-01 15:40:01 +0200

    intent connection parte 2

--- app/src/main/java/com/rainyteam/views/EncyclopediaDetailActivity.kt
+++ app/src/main/java/com/rainyteam/views/EncyclopediaDetailActivity.kt
@@ -4,19 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.rainyteam.controller.MainController
 import com.rainyteam.controller.R
-import com.rainyteam.model.Connection
 import kotlinx.android.synthetic.main.encyclopedia_detail_layout.*
 
 class EncyclopediaDetailActivity : AppCompatActivity() {
 
-    var connection: Connection? = null
+    var mainController: MainController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.encyclopedia_detail_layout)
 
-        this.connection = Connection()
+        this.mainController = intent.extras?.getSerializable("MAIN_CONTROLLER") as MainController
 
         btnBack.setOnClickListener {
             val intent = Intent(this, EncyclopediaActivity::class.java)
@@ -25,7 +25,7 @@ class EncyclopediaDetailActivity : AppCompatActivity() {
 
         val textBenefits: TextView = findViewById(R.id.textBenefitsPlant) as TextView
         textBenefits.setOnClickListener {
-            textBenefits.text = this.connection!!.sendPlantBenefits().toString()
+            textBenefits.text = this.mainController!!.getBenefits().toString()
         }
     }
 
