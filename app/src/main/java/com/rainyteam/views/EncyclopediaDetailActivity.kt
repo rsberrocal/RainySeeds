ee642199bd16adcbf189e3cef94f9d747ddaf77c
Ruben <rubenpa97@gmail.com>
2020-04-05 21:31:37 +0200

    Connection

--- app/src/main/java/com/rainyteam/views/EncyclopediaDetailActivity.kt
+++ app/src/main/java/com/rainyteam/views/EncyclopediaDetailActivity.kt
@@ -12,17 +12,19 @@ import kotlinx.android.synthetic.main.encyclopedia_detail_layout.*
 class EncyclopediaDetailActivity : AppCompatActivity() {
 
     private lateinit var plant: Plants
-    var mainController: Connection? = null
+    var mainConnection: Connection? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.encyclopedia_detail_layout)
 
+        this.mainConnection = Connection()
+
         // plant =
 
         val textBenefits: TextView = findViewById(R.id.textBenefitsPlant)
 
-        textBenefits.text = this.mainController?.sendPlantBenefits().toString()
+        textBenefits.text = mainConnection!!.sendPlantBenefits().toString()
 
         btnBack.setOnClickListener {
             val intent = Intent(this, EncyclopediaActivity::class.java)
