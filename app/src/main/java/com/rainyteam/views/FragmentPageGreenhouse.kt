d8ca19dddbfed609d5e88bcc405e16bf6620147d
Katia <katia.gaag@gmail.com>
2020-04-30 21:33:49 +0200

    accion ir a detail encyclopedia desde planta greenhouse

--- app/src/main/java/com/rainyteam/views/FragmentPageGreenhouse.kt
+++ app/src/main/java/com/rainyteam/views/FragmentPageGreenhouse.kt
@@ -1,5 +1,6 @@
 package com.rainyteam.views
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -62,6 +63,11 @@ class FragmentPageGreenhouse(
                 val resID1: Int =
                     resources.getIdentifier(drawableName1, "drawable", context?.packageName)
                 plantR1C1!!.setImageResource(resID1)
+                plantR1C1.setOnClickListener { view ->
+                    val intent = Intent(plantR1C1.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant1.getName())
+                    plantR1C1.context.startActivity(intent)
+                }
             }
             if (mutableList!!.size > pos * 9 + 1) {
                 val auxPlant2: Plants? =
@@ -70,6 +76,11 @@ class FragmentPageGreenhouse(
                 val resID2: Int =
                     resources.getIdentifier(drawableName2, "drawable", context?.packageName)
                 plantR1C2!!.setImageResource(resID2)
+                plantR1C2.setOnClickListener { view ->
+                    val intent = Intent(plantR1C2.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant2.getName())
+                    plantR1C2.context.startActivity(intent)
+                }
             }
             if (mutableList!!.size > pos * 9 + 2) {
                 val auxPlant3: Plants? =
@@ -78,6 +89,11 @@ class FragmentPageGreenhouse(
                 val resID3: Int =
                     resources.getIdentifier(drawableName3, "drawable", context?.packageName)
                 plantR1C3!!.setImageResource(resID3)
+                plantR1C1.setOnClickListener { view ->
+                    val intent = Intent(plantR1C3.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant3.getName())
+                    plantR1C3.context.startActivity(intent)
+                }
             }
             if (mutableList!!.size > pos * 9 + 3) {
                 val auxPlant4: Plants? =
@@ -86,6 +102,11 @@ class FragmentPageGreenhouse(
                 val resID4: Int =
                     resources.getIdentifier(drawableName4, "drawable", context?.packageName)
                 plantR2C1!!.setImageResource(resID4)
+                plantR2C1.setOnClickListener { view ->
+                    val intent = Intent(plantR2C1.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant4.getName())
+                    plantR2C1.context.startActivity(intent)
+                }
             }
             if (mutableList!!.size > pos * 9 + 4) {
                 val auxPlant5: Plants? =
@@ -94,6 +115,11 @@ class FragmentPageGreenhouse(
                 val resID5: Int =
                     resources.getIdentifier(drawableName5, "drawable", context?.packageName)
                 plantR2C2!!.setImageResource(resID5)
+                plantR2C2.setOnClickListener { view ->
+                    val intent = Intent(plantR2C2.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant5.getName())
+                    plantR2C2.context.startActivity(intent)
+                }
             }
             if (mutableList!!.size > pos * 9 + 5) {
                 val auxPlant6: Plants? =
@@ -102,6 +128,11 @@ class FragmentPageGreenhouse(
                 val resID6: Int =
                     resources.getIdentifier(drawableName6, "drawable", context?.packageName)
                 plantR2C3!!.setImageResource(resID6)
+                plantR2C3.setOnClickListener { view ->
+                    val intent = Intent(plantR2C3.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant6.getName())
+                    plantR2C3.context.startActivity(intent)
+                }
             }
             if (mutableList!!.size > pos * 9 + 6) {
                 val auxPlant7: Plants? =
@@ -110,6 +141,11 @@ class FragmentPageGreenhouse(
                 val resID7: Int =
                     resources.getIdentifier(drawableName7, "drawable", context?.packageName)
                 plantR3C1!!.setImageResource(resID7)
+                plantR3C1.setOnClickListener { view ->
+                    val intent = Intent(plantR3C1.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant7.getName())
+                    plantR3C1.context.startActivity(intent)
+                }
             }
 
             if (mutableList!!.size > pos * 9 + 7) {
@@ -119,6 +155,11 @@ class FragmentPageGreenhouse(
                 val resID8: Int =
                     resources.getIdentifier(drawableName8, "drawable", context?.packageName)
                 plantR3C2!!.setImageResource(resID8)
+                plantR3C2.setOnClickListener { view ->
+                    val intent = Intent(plantR3C2.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant8.getName())
+                    plantR3C2.context.startActivity(intent)
+                }
             }
             if (mutableList!!.size > pos * 9 + 8) {
                 val auxPlant9: Plants? =
@@ -127,6 +168,11 @@ class FragmentPageGreenhouse(
                 val resID9: Int =
                     resources.getIdentifier(drawableName9, "drawable", context?.packageName)
                 plantR3C3!!.setImageResource(resID9)
+                plantR3C3.setOnClickListener { view ->
+                    val intent = Intent(plantR3C3.context, EncyclopediaDetailActivity::class.java)
+                    intent.putExtra("idPlant", auxPlant9.getName())
+                    plantR3C3.context.startActivity(intent)
+                }
             }
         }
         return viewActual
