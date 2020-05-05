40e309fff3e31ae14bd73c23787e82ebc32c4a18
Okabe9 <sergixghost@gmail.com>
2020-05-05 09:17:32 +0200

    Water Animations finished(missing to fix bottle size bug)

--- app/src/main/java/com/rainyteam/views/MainWaterActivity.kt
+++ app/src/main/java/com/rainyteam/views/MainWaterActivity.kt
@@ -8,14 +8,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import com.rainyteam.controller.R
 import com.rainyteam.model.Connection
 import com.rainyteam.model.History
 import kotlinx.android.synthetic.main.main_water_layout.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import java.nio.channels.Channel
 import kotlin.coroutines.CoroutineContext
 
 
@@ -26,7 +25,6 @@ class MainWaterActivity : AppCompatActivity(), CoroutineScope {
     val PREF_NAME = "USER"
     var prefs: SharedPreferences? = null
     var userName: String? = null
-
     var textWaterPercent: TextView? = null
 
     private var job: Job = Job()
@@ -47,7 +45,7 @@ class MainWaterActivity : AppCompatActivity(), CoroutineScope {
         this.userName = prefs!!.getString("USER_ID", "")
 
         this.mainConnection = Connection()
-        setWaterImage(userName)
+        async{ getWater(userName)}
         setUser(userName)
         this.textWaterPercent = findViewById(R.id.waterPercent)
         launch {
@@ -75,13 +73,13 @@ class MainWaterActivity : AppCompatActivity(), CoroutineScope {
         }
     }
     @RequiresApi(Build.VERSION_CODES.N)
-    fun setWaterImage(user: String?) {
+    fun getWater(user: String?) {
 
-        var actualUserWater: Float = 0.0f
         launch {
             var actualHistory: History? = mainConnection!!.getHistory(user!!)
             var cal: Calendar = Calendar.getInstance()
             var day = cal.get(Calendar.DAY_OF_WEEK)
+            var actualUserWater: Float = 30f
             when (day) {
                 Calendar.SUNDAY -> actualUserWater = actualHistory!!.sunday
                 Calendar.MONDAY -> actualUserWater = actualHistory!!.monday
@@ -91,6 +89,37 @@ class MainWaterActivity : AppCompatActivity(), CoroutineScope {
                 Calendar.FRIDAY -> actualUserWater = actualHistory!!.friday
                 Calendar.SATURDAY -> actualUserWater = actualHistory!!.saturday
             }
+            setWaterImage(actualUserWater)
         }
+
     }
+
+    fun setWaterImage(actualUserWater: Float){
+        when{
+            actualUserWater <= 10 && actualUserWater > 5 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_5))
+            actualUserWater <= 20 && actualUserWater > 10 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_10))
+            actualUserWater <= 30 && actualUserWater > 20 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_20))
+            actualUserWater <= 40 && actualUserWater > 30 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_30))
+            actualUserWater <= 50 && actualUserWater > 40 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_40))
+            actualUserWater <= 60 && actualUserWater > 50 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_50))
+            actualUserWater <= 70 && actualUserWater > 60 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_60))
+            actualUserWater <= 80 && actualUserWater > 70 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_70))
+            actualUserWater <= 90 && actualUserWater > 80 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_80))
+            actualUserWater <= 95 && actualUserWater > 90 -> waterButton.setImageDrawable(
+                ContextCompat.getDrawable(getApplicationContext(),R.drawable.bottle_90))
+        }
+        textWaterPercent!!.text = actualUserWater.toString()+"%"
+
+    }
+
+
 }
