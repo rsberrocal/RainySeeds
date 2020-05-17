ee6a836b9efb65930fe73e4bfdb7f896a77fae79
Katia <katia.gaag@gmail.com>
2020-05-17 18:10:31 +0200

    info user

--- app/src/main/java/com/rainyteam/views/FragmentViewMenu.kt
+++ app/src/main/java/com/rainyteam/views/FragmentViewMenu.kt
@@ -21,13 +21,13 @@ class FragmentViewMenu : androidx.fragment.app.Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        prefs = activity!!.getSharedPreferences(PREF_ID, 0)
+        prefs = requireActivity().getSharedPreferences(PREF_ID, 0)
         //prefs!!.edit().putBoolean("NAV", false).apply()
 
         val viewActual = inflater.inflate(R.layout.fragment_viewmenu, container, false)
 
         viewActual.buttonDictionary.setOnClickListener { view ->
-            activity!!.finish()
+            requireActivity().finish()
             if (activity?.javaClass?.simpleName != EncyclopediaActivity::class.simpleName) {
                 val principal = Intent(activity, EncyclopediaActivity::class.java)
                 prefs!!.edit().putBoolean("NAV",true).apply()
@@ -37,7 +37,7 @@ class FragmentViewMenu : androidx.fragment.app.Fragment() {
         }
 
         viewActual.buttonGlass.setOnClickListener { view ->
-                activity!!.finish()
+                requireActivity().finish()
             println(activity?.javaClass?.simpleName)
             if (activity?.javaClass?.simpleName != MainWaterActivity::class.simpleName) {
                 val principal = Intent(activity, MainWaterActivity::class.java)
@@ -48,7 +48,7 @@ class FragmentViewMenu : androidx.fragment.app.Fragment() {
         }
         viewActual.buttonGreenhouse.setOnClickListener { view ->
             println(activity?.javaClass?.simpleName)
-                activity!!.finish()
+                requireActivity().finish()
             if (activity?.javaClass?.simpleName != GreenhouseActivity::class.simpleName) {
                 val principal = Intent(activity, GreenhouseActivity::class.java)
                 prefs!!.edit().putBoolean("NAV",true).apply()
